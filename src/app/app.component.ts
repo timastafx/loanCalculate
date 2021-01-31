079aa6ceb24a234299cb274c0d3c93f05bfce757
Shepel <andrey.shepel@veloceapps.com>
2021-01-31 20:49:34 +0500

    [Add] Components.module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Tab } from '../../components/src/tabs/tabs.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'loanCalculate';
+
+  tabs: Tab[] = [
+    {
+      id: '0',
+      label: 'Кредитный калькулятор',
+      path: ['/loanCalculate']
+    },
+    {
+      id: '1',
+      label: 'Расчет портфеля',
+      path: ['/quantification']
+    }
+  ];
 }
