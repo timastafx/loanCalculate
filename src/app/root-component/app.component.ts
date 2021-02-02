5f65ff8ceaf3ff3e398372ac90d208adfc32111d
Andrey <shepel.a.m@gmail.com>
2021-02-02 06:05:47 +0500

    Finished theming

--- src/app/root-component/app.component.ts
+++ src/app/root-component/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Tab } from '../../../components/src/tabs/tabs.component';
 
 export enum Theme {
@@ -15,13 +15,16 @@ const themesClassMap = {
   [Theme.Color]: `${themeClassName}--color`
 };
 
+const localStorageThemeName = 'Theme';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  theme = Theme.Dark;
+export class AppComponent implements OnInit {
+  activeTheme = Theme.Light;
+  themeValues = Object.values(Theme);
 
   tabs: Tab[] = [
     {
@@ -38,7 +41,16 @@ export class AppComponent {
 
   get themeClass(): { [key: string]: boolean } {
     return {
-      [themesClassMap[this.theme]]: true
+      [themesClassMap[this.activeTheme]]: true
     };
+  }
+
+  ngOnInit(): void {
+    this.activeTheme = localStorage.getItem(localStorageThemeName) as Theme;
+  }
+
+  onThemeChange(theme: Theme): void {
+    this.activeTheme = theme;
+    localStorage.setItem(localStorageThemeName, theme);
   }
 }
